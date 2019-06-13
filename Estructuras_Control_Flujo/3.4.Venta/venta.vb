9dce723b3938b1b05601a07873f4eaacc309fba4
Gonzalo Techeira <gnxa95@hotmail.com>
2019-06-12 22:21:19 -0300

    ...

--- Estructuras_Control_Flujo/3.4.Venta/venta.vb
+++ Estructuras_Control_Flujo/3.4.Venta/venta.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim cantidad, precio_unitario, monto_descontado As Integer
+        Dim cantidad, descuento, precio_unitario, monto_descontado As Integer
         Dim total_productos As Integer = 0
 
         Console.WriteLine("Ingrese la cantidad de productos: ")
@@ -10,20 +10,18 @@ Module Program
         Console.WriteLine("Ingrese el precio del producto: ")
         precio_unitario = Console.ReadLine()
         total_productos = cantidad * precio_unitario
-
         Console.WriteLine("El subtotal es de: $" & total_productos)
 
         If cantidad > 9 And cantidad < 51 Then
-            Console.WriteLine("Se desconto el %5")
-            monto_descontado = (total_productos * 5) / 100
+            descuento = 5
         ElseIf cantidad > 50 And cantidad < 251 Then
-            Console.WriteLine("Se desconto el 10%")
-            monto_descontado = (total_productos * 10) / 100
+            descuento = 10
         ElseIf cantidad > 250 Then
-            Console.WriteLine("Se desconto el 20%")
-            monto_descontado = (total_productos * 15) / 100
+            descuento = 20
         End If
 
+        monto_descontado = (total_productos * descuento) / 100
+        Console.WriteLine("Se desconto un {0}%", descuento)
         Console.WriteLine("El monto descontado es de: $" & monto_descontado)
         Console.WriteLine("El total a pagar es de: $" & (total_productos - monto_descontado))
     End Sub
